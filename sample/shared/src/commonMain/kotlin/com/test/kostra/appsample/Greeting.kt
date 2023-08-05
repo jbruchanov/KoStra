8cdac1b297c0fbfba8859aee8ec105c686fd2f4e
Jiri Bruchanov <jbruchanov@gmail.com>
2023-08-05 13:19:08 +0100

    KS-30 Update project structure

--- sample/shared/src/commonMain/kotlin/com/test/kostra/appsample/Greeting.kt
+++ sample/shared/src/commonMain/kotlin/com/test/kostra/appsample/Greeting.kt
@@ -1,6 +1,7 @@
 package com.test.kostra.appsample
 
 import com.jibru.kostra.AssetResourceKey
+import com.jibru.kostra.BinaryResourceKey
 import com.jibru.kostra.DrawableResourceKey
 import com.jibru.kostra.PluralResourceKey
 import com.jibru.kostra.ResourceContainer
@@ -14,7 +15,7 @@ class Greeting {
         override val string: Map<StringResourceKey, ResourceContainer> = emptyMap()
         override val plural: Map<PluralResourceKey, ResourceContainer> = emptyMap()
         override val drawable: Map<DrawableResourceKey, ResourceContainer> = emptyMap()
-        override val binary: Map<AssetResourceKey, ResourceContainer> = emptyMap()
+        override val binary: Map<BinaryResourceKey, ResourceContainer> = emptyMap()
     }
 
     fun greet(): String {
