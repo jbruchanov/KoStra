ba13d54411852c45cf27d233ea8ee4ff701e19c6
Jiri Bruchanov <jbruchanov@gmail.com>
2023-08-07 08:45:32 +0100

    KS-32 Refactor Locale to use data class with simple int as code

--- lib-kostra-common/src/commonMain/kotlin/com/jibru/kostra/internal/Locale.kt
+++ lib-kostra-common/src/commonMain/kotlin/com/jibru/kostra/internal/Locale.kt
@@ -1,46 +1,86 @@
+@file:Suppress("ReplaceManualRangeWithIndicesCalls")
+
 package com.jibru.kostra.internal
 
 import com.jibru.kostra.internal.ext.takeIfNotEmpty
 
-class Locale(language: String, region: String?) {
+private const val LetterCodeMin = 'a'.code
+private const val LetterCodeMax = 'z'.code
+
+/*-1 to have a as 1, not 0, to avoid clashing with 0 as undefined*/
+private const val LetterOffset = LetterCodeMin - 1
+private val LocaleOffsets = intArrayOf(1_00_00_00_00, 1_00_00_00, 1_00_00, 1_00, 1)
+
+@JvmInline
+value class Locale(val key: Int) {
 
     constructor(languageRegion: String) : this(languageRegion.substringBefore("-"), languageRegion.substringAfter("-", "").takeIfNotEmpty())
+    constructor(language: String, region: String?) : this(packLocale(language, region))
 
-    val language: String = language.lowercase()
-    val region: String? = region?.lowercase()?.let { if (it.startsWith("r")) it.substring(1) else it }
-    val languageRegion = if (this.region == null) this.language else "${this.language}${this.region}"
+    fun equalsLanguage(other: Locale): Boolean = this.languageCode == other.languageCode
 
-    init {
-        if (language.isNotEmpty() && region != null) {
-            require(region.isNotEmpty()) { "Empty region" }
+    val language: String
+        get() {
+            if (key == 0) return ""
+            return buildString(0, 2)
+        }
+
+    val region: String?
+        get() {
+            if (key % LocaleOffsets[2] == 0) return null
+            return buildString(2, 4)
+        }
+
+    val languageRegion
+        get() = buildString {
+            append(language)
+            region?.let { append(it) }
+        }
+
+    internal val languageCode: Int
+        get() = key / LocaleOffsets[1]
+
+    internal val regionCode: Int
+        get() = key % LocaleOffsets[1]
+
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun buildString(from: Int, toExclusive: Int): String = buildString {
+        for (i in from..<toExclusive) {
+            append((LetterOffset + (key % LocaleOffsets[i] / (LocaleOffsets.getOrNull(i + 1) ?: 1))).toChar())
         }
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Locale
-
-        if (language != other.language) return false
-        if (region != other.region) return false
-
-        return true
-    }
-
-    fun equalsLanguage(other: Locale) = this.language == other.language
-
-    override fun hashCode(): Int {
-        var result = language.hashCode()
-        result = 31 * result + (region?.hashCode() ?: 0)
-        return result
-    }
-
     override fun toString(): String {
-        return if (this == Undefined) "Locale.Undefined" else "Locale(l='$language', r=$region)"
+        return if (key == 0) "Locale.Undefined" else "Locale(l='$language', r=$region)"
     }
 
     companion object {
-        val Undefined = Locale("", null)
+        val Undefined = Locale(0)
+
+        //ceil(log(('z' - 'a' + 1).pow(4), 2.0))
+        //can be optimised down if it's taken to binary math, instead of decimal
+        const val Bits = 20
     }
+}
+
+@Suppress("NAME_SHADOWING")
+private fun packLocale(language: String, region: String?): Int {
+    val region = if (region?.getOrNull(0) == 'r') region.substring(1) else region
+    require(language.isEmpty() || language.length == 2) { "Invalid language:'$language', must be 0 or 2 chars!" }
+    require(region.isNullOrEmpty() || region.length == 2) { "Invalid region:'$region', must be null, 0 or 2 chars!" }
+    if (language.isEmpty()) return 0
+    if (language.isEmpty()) return 0
+    val result =
+        ((language[0].validCode() - LetterOffset) * LocaleOffsets[1]) +
+            ((language[1].validCode() - LetterOffset) * LocaleOffsets[2]) +
+            (((region?.getOrNull(0)?.validCode() ?: LetterOffset) - LetterOffset) * LocaleOffsets[3]) +
+            (((region?.getOrNull(1)?.validCode() ?: LetterOffset) - LetterOffset) * LocaleOffsets[4])
+    return result
+}
+
+@Suppress("ConvertTwoComparisonsToRangeCheck")
+private fun Char.validCode(): Int {
+    val c = code
+    val code = if (c < LetterOffset) c + 32/*'A' vs 'a' offset*/ else c
+    return requireNotNull(code.takeIf { LetterCodeMin <= it && it <= LetterCodeMax }) { "Invalid locale char:'$this must be 'a'-'z'!" }
 }
