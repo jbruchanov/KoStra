e9992995c3b665345fef189179584126d7d35c59
Jiri Bruchanov <jbruchanov@gmail.com>
2023-08-14 09:19:05 +0100

    KS-40  Add gradle file watcher to update source code right away

--- kostra-gradle-plugin/src/main/kotlin/com/jibru/kostra/plugin/KostraPlugin.kt
+++ kostra-gradle-plugin/src/main/kotlin/com/jibru/kostra/plugin/KostraPlugin.kt
@@ -1,17 +1,26 @@
 @file:Suppress("unused")
-@file:OptIn(ExperimentalTime::class)
+@file:OptIn(ExperimentalTime::class, DelicateCoroutinesApi::class, FlowPreview::class)
 
 package com.jibru.kostra.plugin
 
 import com.jibru.kostra.plugin.task.AnalyseResourcesTask
 import com.jibru.kostra.plugin.task.GenerateCodeTask
 import com.jibru.kostra.plugin.task.GenerateDatabasesTask
+import com.jibru.kostra.plugin.task.TaskDelegate
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.launch
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.slf4j.LoggerFactory
 import java.io.File
+import java.time.LocalDateTime
 import kotlin.time.ExperimentalTime
 
 class KostraPlugin : Plugin<Project> {
@@ -22,7 +31,7 @@ class KostraPlugin : Plugin<Project> {
         val extension = target.extensions.create("kostra", KostraPluginExtension::class.java)
 
         val analyseResources = target.tasks.register("analyseResources", AnalyseResourcesTask::class.java) {
-            it.outputFile.set(File(extension.output.get(), "resources.obj"))
+            it.outputFile.set(File(extension.outputDir.get(), "resources.obj"))
             it.resourceDirs.set(extension.resourceDirs)
         }
 
@@ -30,13 +39,13 @@ class KostraPlugin : Plugin<Project> {
             task.packageName.set(extension.packageName)
             task.kClassName.set(extension.kClassName)
             task.resources.set(analyseResources.flatMap { it.outputFile })
-            task.output.set(extension.output.map { File(it, "src") })
+            task.output.set(extension.outputDir.map { File(it, "src") })
             task.dependsOn(analyseResources)
         }
 
         val generateDatabasesTaskTaskProvider = target.tasks.register("generateDatabases", GenerateDatabasesTask::class.java) {
             it.resources.set(analyseResources.flatMap { it.outputFile })
-            it.output.set(extension.output.map { File(it, "resources/__kostra") })
+            it.output.set(extension.outputDir.map { File(it, "resources/__kostra") })
             it.dependsOn(analyseResources)
         }
 
@@ -59,15 +68,17 @@ class KostraPlugin : Plugin<Project> {
 
         extension.apply {
             autoConfig.set(true)
+            useFileWatcher.set(false)
             packageName.set("com.jibru.kostra")
             kClassName.set("K")
-            output.set(File(target.buildDir, "kostra"))
+            outputDir.set(File(target.buildDir, "kostra"))
         }
 
         target.afterEvaluate { project ->
             if (extension.autoConfig.get()) {
                 tryUpdateSourceSets(target, project, extension)
             }
+            updateFileWatcher(target, extension)
         }
     }
 
@@ -109,6 +120,56 @@ class KostraPlugin : Plugin<Project> {
                     commonMainSourceSet.resources.srcDir(resourcesDir)
                     logger.info("KMP added resources:${commonMainSourceSet.resources.srcDirs.joinToString()}")
                 }
+        }
+    }
+
+    private var fileWatcher = FileWatcher()
+    private var fileWatcherJob: Job? = null
+    private fun updateFileWatcher(target: Project, extension: KostraPluginExtension) {
+        fileWatcherJob?.cancel()
+        fileWatcherJob = null
+        val folders = extension.resourceDirs.get().filter { it.isDirectory }
+        if (folders.isNotEmpty() && extension.useFileWatcher.get()) {
+            val taskDelegateConfig = TaskDelegate.Config(
+                resourceDirs = extension.resourceDirs.get(),
+                fileResolverConfig = extension.androidResources.toFileResolverConfig(),
+                packageName = extension.packageName.get(),
+                kClassName = extension.kClassName.get(),
+                output = File(extension.outputDir.get(), "src"),
+            )
+            val log = File(target.buildDir, "kostra/filewatcher.log")
+            fileWatcherJob = GlobalScope.launch(Dispatchers.IO) {
+                fileWatcher.flowChanges(folders)
+                    .debounce(1000L)
+                    .collect {
+                        log.appendText("${LocalDateTime.now()}\n")
+                        onFileWatchedNotified(log = null, taskDelegateConfig)
+                    }
+            }
+        }
+    }
+
+    private fun onFileWatchedNotified(
+        log: File?,
+        taskDelegateConfig: TaskDelegate.Config,
+    ) = with(TaskDelegate) {
+        runCatching {
+            val items = analyseCode(
+                resourceDirs = taskDelegateConfig.resourceDirs,
+                fileResolverConfig = taskDelegateConfig.fileResolverConfig,
+            )
+            generateCode(
+                packageName = taskDelegateConfig.packageName,
+                kClassName = taskDelegateConfig.kClassName,
+                items = items,
+                output = taskDelegateConfig.output,
+            )
+        }.exceptionOrNull()?.also {
+            log?.let { log ->
+                log.appendText((it.message ?: "null") + "\n")
+                log.appendText(it.stackTraceToString())
+                log.appendText("\n")
+            }
         }
     }
 }
