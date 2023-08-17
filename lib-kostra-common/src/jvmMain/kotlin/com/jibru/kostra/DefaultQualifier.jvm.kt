8ef289d642b61123ece570b51a255bc730e17ed0
Jiri Bruchanov <jbruchanov@gmail.com>
2023-08-17 10:47:38 +0100

    KS-27 Fix defaultDpi check on JVM with no display

--- lib-kostra-common/src/jvmMain/kotlin/com/jibru/kostra/DefaultQualifier.jvm.kt
+++ lib-kostra-common/src/jvmMain/kotlin/com/jibru/kostra/DefaultQualifier.jvm.kt
@@ -8,12 +8,13 @@ import java.util.Locale as JvmLocale
 
 actual fun defaultQualifiers(): Qualifiers = Qualifiers(
     locale = JvmLocale.getDefault().let { Locale(it.language, it.country.takeIf { code -> code.isEmpty() || code.length == 2 }) },
-    dpi = Dpi.getClosest(
-        //taken from JVM LocalDensity
-        GraphicsEnvironment.getLocalGraphicsEnvironment()
-            .defaultScreenDevice
-            .defaultConfiguration
-            .defaultTransform
-            .scaleX.toFloat(),
-    ),
+    //taken from JVM LocalDensity
+    dpi = GraphicsEnvironment.getLocalGraphicsEnvironment()
+        ?.takeIf { !it.isHeadlessInstance }
+        ?.defaultScreenDevice
+        ?.defaultConfiguration
+        ?.defaultTransform
+        ?.scaleX?.toFloat()
+        ?.let { Dpi.getClosest(it) }
+        ?: Dpi.Undefined,
 )
