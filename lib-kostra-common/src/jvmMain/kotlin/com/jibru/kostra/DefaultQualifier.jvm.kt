561f72bde1cb989572e67347adbfc8226edc9c16
Jiri Bruchanov <jbruchanov@gmail.com>
2023-08-17 10:28:08 +0100

    KS-27 Add kostra-compose module

--- lib-kostra-common/src/jvmMain/kotlin/com/jibru/kostra/DefaultQualifier.jvm.kt
+++ lib-kostra-common/src/jvmMain/kotlin/com/jibru/kostra/DefaultQualifier.jvm.kt
@@ -3,9 +3,17 @@ package com.jibru.kostra
 import com.jibru.kostra.internal.Dpi
 import com.jibru.kostra.internal.Locale
 import com.jibru.kostra.internal.Qualifiers
+import java.awt.GraphicsEnvironment
 import java.util.Locale as JvmLocale
 
 actual fun defaultQualifiers(): Qualifiers = Qualifiers(
     locale = JvmLocale.getDefault().let { Locale(it.language, it.country.takeIf { code -> code.isEmpty() || code.length == 2 }) },
-    dpi = Dpi.Undefined,
+    dpi = Dpi.getClosest(
+        //taken from JVM LocalDensity
+        GraphicsEnvironment.getLocalGraphicsEnvironment()
+            .defaultScreenDevice
+            .defaultConfiguration
+            .defaultTransform
+            .scaleX.toFloat(),
+    ),
 )
