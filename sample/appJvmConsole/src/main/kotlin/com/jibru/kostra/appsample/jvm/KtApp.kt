18c5066c32a7121ed293baa027a095e18012a469
Jiri Bruchanov <jbruchanov@gmail.com>
2023-09-05 08:06:01 +0100

    KS-18 Improve keyMapperKt

--- sample/appJvmConsole/src/main/kotlin/com/jibru/kostra/appsample/jvm/KtApp.kt
+++ sample/appJvmConsole/src/main/kotlin/com/jibru/kostra/appsample/jvm/KtApp.kt
@@ -18,27 +18,27 @@ fun main() {
         println("-".repeat(32))
         println("Current locale:${Locale.getDefault()}")
         println("Strings:")
-        val items = listOf(K.string.action_add, K.string.action_remove, K.string.color, K.string.plurals, K.string.ordinals)
+        val items = listOf(K.string.actionAdd, K.string.actionRemove, K.string.color, K.string.plurals, K.string.ordinals)
         println(items.joinToString { Resources.string(it) })
         println("Plurals:")
         println(
             listOf(
-                Resources.plural(K.plural.bug_x, 0, 0f),
-                Resources.plural(K.plural.bug_x, FixedDecimal(0.5), 0.5f),
-                Resources.plural(K.plural.bug_x, 1, 1),
-                Resources.plural(K.plural.bug_x, 10, 10)
+                Resources.plural(K.plural.bugX, 0, 0f),
+                Resources.plural(K.plural.bugX, FixedDecimal(0.5), 0.5f),
+                Resources.plural(K.plural.bugX, 1, 1),
+                Resources.plural(K.plural.bugX, 10, 10)
             ).joinToString()
         )
         println("Ordinals:")
-        println((0..5).joinToString { Resources.ordinal(K.plural.day_x, it, it) })
+        println((0..5).joinToString { Resources.ordinal(K.plural.dayX, it, it) })
 
-        ImageIO.read(Resources.binaryInputStream(K.drawable.capital_city)).also {
-            val assetPath = Resources.assetPath(K.drawable.capital_city)
+        ImageIO.read(Resources.binaryInputStream(K.drawable.capitalCity)).also {
+            val assetPath = Resources.assetPath(K.drawable.capitalCity)
             println("$assetPath imageRes:${it.width}x${it.height}")
         }
         val xxHdpiQualifiers = defaultQualifiers().copy(dpi = KDpi.XXHDPI)
-        ImageIO.read(Resources.binaryInputStream(K.drawable.capital_city, qualifiers = xxHdpiQualifiers)).also {
-            val assetPath = Resources.assetPath(K.drawable.capital_city, xxHdpiQualifiers)
+        ImageIO.read(Resources.binaryInputStream(K.drawable.capitalCity, qualifiers = xxHdpiQualifiers)).also {
+            val assetPath = Resources.assetPath(K.drawable.capitalCity, xxHdpiQualifiers)
             if (it != null) {
                 println("$assetPath imageRes:${it.width}x${it.height}")
             } else {
