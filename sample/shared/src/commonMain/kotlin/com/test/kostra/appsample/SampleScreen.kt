96ffb27344d9fe3fbc507debd20031fc2fff65af
Jiri Bruchanov <jbruchanov@gmail.com>
2023-10-04 08:52:42 +0100

    test

--- sample/shared/src/commonMain/kotlin/com/test/kostra/appsample/SampleScreen.kt
+++ sample/shared/src/commonMain/kotlin/com/test/kostra/appsample/SampleScreen.kt
@@ -2,176 +2,65 @@
 
 package com.test.kostra.appsample
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.material.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
-import com.jibru.kostra.KDpi
-import com.jibru.kostra.KLocale
-import com.jibru.kostra.KQualifiers
-import com.jibru.kostra.compose.LocalQualifiers
-import com.jibru.kostra.icu.FixedDecimal
-import com.sample.app.K
-import com.sample.app.compose.assetPath
-import com.sample.app.compose.get
-import com.sample.app.compose.ordinalResource
-import com.sample.app.compose.painterResource
-import com.sample.app.compose.pluralResource
-import com.sample.app.compose.stringResource
-import com.sample.lib1.compose.get
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 
 private object SampleScreenDefaults {
     val spacing = 8.dp
 }
 
 @Composable
-fun SampleScreen(extraContent: @Composable ColumnScope.() -> Unit = {}) = with(SampleScreenDefaults) {
-    Box(
-        modifier = Modifier
-            .verticalScroll(rememberScrollState())
-            .padding(spacing),
-    ) {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(spacing),
-            modifier = Modifier
-                .padding(spacing)
-                .animateContentSize()
-                .wrapContentSize(),
-        ) {
-            val defaultQualifiers = LocalQualifiers.current
-            val locales = remember {
-                val codes = listOf("ar", "cs", "en", "enGB", "enUS", "he", "hi", "ja", "ko", "ru", "th")
-                (listOf(defaultQualifiers.locale) + codes.map { KLocale(it) }).distinct()
-            }
-            var localeIndex by remember { mutableStateOf(0) }
-            val locale by remember { derivedStateOf { KLocale(locales[localeIndex].languageRegion) } }
+fun AnnotatedText() {
+    val sampleText = "SAMPLE text 12345"
+    val text = buildAnnotatedString {
+        appendLine(sampleText)
+        withStyle(SpanStyle(fontSize = 0.5f.em)) {
+            appendLine("$sampleText 0.5em")
+        }
+        withStyle(SpanStyle(fontSize = 1f.em)) {
+            appendLine("$sampleText 1em")
+        }
+        withStyle(SpanStyle(fontSize = 2.em)) {
+            appendLine("$sampleText 2em")
+        }
 
-            val dpis = remember { listOf(null, KDpi.Undefined, KDpi.XHDPI, KDpi.XXHDPI) }
-            var dpiIndex by remember { mutableStateOf(0) }
-            val dpi by remember { derivedStateOf { dpis[dpiIndex] ?: defaultQualifiers.dpi } }
-
-            val qualifiers by remember { derivedStateOf { KQualifiers(locale, dpi) } }
-            CompositionLocalProvider(LocalQualifiers provides qualifiers) {
-                Text("KQualifiers:$defaultQualifiers")
-                Text("KLocale")
-                FlowRow(
-                    verticalArrangement = Arrangement.Center,
-                ) {
-                    locales.forEachIndexed { index, locale ->
-                        TextCheckBox(
-                            onCheckedChange = { if (it) localeIndex = index else Unit },
-                            checked = index == localeIndex,
-                            text = locale.languageRegion,
-                            modifier = Modifier.weight(1f),
-                        )
-                    }
-                }
-
-                Text("DPI: ${defaultQualifiers.dpi}")
-                Text("Density:${LocalDensity.current}")
-                FlowRow(
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxWidth(),
-                ) {
-                    dpis.forEachIndexed { index, dpi ->
-                        val text = buildAnnotatedString {
-                            append(dpi?.name ?: "Dev:${defaultQualifiers.dpi.name}")
-                            if (dpi == KDpi.XXHDPI) {
-                                withStyle(SpanStyle(fontSize = TextUnit(0.5f, TextUnitType.Em))) {
-                                    append("\n(most of capital_city asset)")
-                                }
-                            }
-                        }
-
-                        TextCheckBox(
-                            onCheckedChange = { if (it) dpiIndex = index else Unit },
-                            checked = index == dpiIndex,
-                            text = text,
-                            modifier = Modifier.weight(1f),
-                        )
-                    }
-                }
-
-                FlowRow(horizontalArrangement = Arrangement.spacedBy(spacing)) {
-                    Button(onClick = {}) { Text(stringResource(K.string.action_add)) }
-                    Button(onClick = {}) { Text(stringResource(K.string.action_remove)) }
-                    Button(onClick = {}) { Text(KM.string.color.get()) }
-                    Button(onClick = {}) { Text(KM.string.lib1_text.get()) }
-                    //Lib2 is not released with compose dependency, `get()` must be done manually
-                    Button(onClick = {}) { Text(KM.string.lib2_text.get()) }
-                }
-
-                Text(assetPath(K.images.capital_city))
-                Image(painterResource(K.images.capital_city), contentDescription = null)
-
-                Spacer(modifier = Modifier.height(4.dp))
-
-                Row {
-                    Image(KM.images.gear1.get(), contentDescription = null)
-                    Image(KM.images.gear2.get(), contentDescription = null)
-                }
-
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(assetPath(K.flagsxml.country_flag))
-                Image(painterResource(K.flagsxml.country_flag), contentDescription = null, modifier = Modifier.height(64.dp))
-
-                extraContent()
-
-                var quantity by remember { mutableStateOf("1") }
-
-                TextField(
-                    value = quantity,
-                    label = { Text(K.string.plurals.get(1, 2, 3)) },
-                    placeholder = { Text(stringResource(K.string.type_quantity)) },
-                    onValueChange = { quantity = it.takeIf { it.isEmpty() || it.toFloatOrNull() != null } ?: quantity },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                )
-
-                Text(stringResource(K.string.plurals) + ": " + (quantity.toDoubleOrNull()?.let { pluralResource(K.plural.bug_x, FixedDecimal(it), quantity) } ?: ""))
-                Text(stringResource(K.string.ordinals) + ": " + (quantity.toDoubleOrNull()?.let { ordinalResource(K.plural.day_x, FixedDecimal(it), quantity) } ?: ""))
-            }
+        withStyle(SpanStyle(fontSize = 7.sp)) {
+            appendLine("$sampleText 7sp")
+        }
+        withStyle(SpanStyle(fontSize = 14.sp)) {
+            appendLine("$sampleText 14sp")
+        }
+        withStyle(SpanStyle(fontSize = 28.sp)) {
+            appendLine("$sampleText 28sp")
         }
     }
+    Text(text, fontSize = 14.sp)
+}
+
+@Composable
+fun SampleScreen(extraContent: @Composable ColumnScope.() -> Unit = {}) = with(SampleScreenDefaults) {
+    AnnotatedText()
 }
 
 @Composable
