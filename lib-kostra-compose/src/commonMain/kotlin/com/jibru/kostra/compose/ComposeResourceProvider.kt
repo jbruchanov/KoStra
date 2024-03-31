99d2e4dfe54e4d16c7df806b3a2d0024943f936a
Jiri Bruchanov <jbruchanov@gmail.com>
2024-04-01 00:10:16 +0100

    KS-10 Fix ingoring vxml as vector drawable when loading via composePainter

--- lib-kostra-compose/src/commonMain/kotlin/com/jibru/kostra/compose/ComposeResourceProvider.kt
+++ lib-kostra-compose/src/commonMain/kotlin/com/jibru/kostra/compose/ComposeResourceProvider.kt
@@ -4,19 +4,30 @@
 package com.jibru.kostra.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import com.jibru.kostra.KQualifiers
 import com.jibru.kostra.KResources
 import com.jibru.kostra.PainterResourceKey
 import com.jibru.kostra.assetPath
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.imageResource
+import org.jetbrains.compose.resources.vectorResource
 
 @Composable
 internal fun KResources.composePainter(key: PainterResourceKey, qualifiers: KQualifiers): Painter {
     val path = assetPath(key, qualifiers)
     require(!path.endsWith(".svg", ignoreCase = true)) { "Unsupported SVG on current platform, key:$key, asset:'$path'" }
-    return org.jetbrains.compose.resources.painterResource(DrawableResource(path))
+    val isXml = path.endsWith(".xml") || path.endsWith(".vxml")
+    //imageResources is caching stuff internally
+    //it's a copy of org.jetbrains.compose.resources.painterResource, just using also vxml for the vector drawables
+    if (isXml) {
+        return rememberVectorPainter(vectorResource(DrawableResource(path)))
+    } else {
+        return BitmapPainter(imageResource(DrawableResource(path)))
+    }
 }
 
 @Composable
